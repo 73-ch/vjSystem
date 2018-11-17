6a7eecfb818d7cd0eb85c5f28773ebb9c703e34d
nami634 <cigarbox.nami@gmail.com>
2018-11-18 04:16:18 +0900

    ウィンドウの設定を追加

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,16 @@
 
 //========================================================================
 int main( ){
+    
+    // システム環境設定で、ウィンドウの配置も行いつつ、調整を行う
     ofGLWindowSettings settings;
     settings.setGLVersion(3, 2);
-    settings.setPosition(glm::vec2(0));
-    settings.setSize(1024, 768);
+    settings.setPosition(glm::vec2(2880, 0));// 自分のパソコンを考慮したポジション
+    settings.setSize(1920, 1080);// ここに会場のプロジェクターの解像度
     ofCreateWindow(settings);
+
+    ofSetFullscreen(true);
+    
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
